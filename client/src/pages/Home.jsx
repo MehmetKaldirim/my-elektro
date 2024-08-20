f90f6042d3c5bf48c8ec35015f6ca31419ee90b3
Mehmet <mathdenizi@gmail.com>
2024-08-20 19:54:38 +0200

    wie added to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import UnsereLeistungen from "../components/UnsereLeistungen";
+import Wie from "../components/Wie";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <UnsereLeistungen />
+      <Wie />
     </div>
   );
 }
